8af53f85dcd059433e19066c19919cfc12b1867e
Kaisa Löfman <kaisalo@metropolia.fi>
2018-03-05 23:12:16 +0200

    function to change whos notes are showing

--- CSS & HTML & JS/torni.js
+++ CSS & HTML & JS/torni.js
@@ -19,9 +19,9 @@ document.addEventListener("DOMContentLoaded", function () {
 	}
 
 	//const allnotes = document.getElementById("allnotes");
-//	allnotes.addEventListener("click", function(){
-//	})
-	
+	//	allnotes.addEventListener("click", function(){
+	//	})
+
 });
 
 //document.addEventListener("DOMContenLoaded")
@@ -34,6 +34,8 @@ fetch(url)
 		return jsObj;
 	});
 */
+
+//Checks if user wants to close the note
 function noteFinished() {
 	let txt;
 	let r = confirm("Are you sure the task is finished?");
@@ -43,4 +45,45 @@ function noteFinished() {
 		txt = "You pressed Cancel";
 	}
 	document.getElementById("closing").innerHTML = txt;
+}
+
+//This function changes whos notes are showing
+function showNotes(group) {
+	let change = document.getElementById(group).id;
+	console.log(change);
+		
+	switch (change) {
+		case 'allnotes': document.getElementById('notesGroup').innerHTML = "All notes";
+			console.log('all');
+			break;
+		case 'bar': document.getElementById('notesGroup').innerHTML = "Bar"
+			console.log('bar');
+			break;
+		case 'breakfast':document.getElementById('notesGroup').innerHTML = "Breakfast";
+			console.log('breku');
+			break;
+		case 'floorcare': document.getElementById('notesGroup').innerHTML = "Floorcare";
+			console.log('floorcare');
+			break;
+		case 'hotelmanager':document.getElementById('notesGroup').innerHTML = "Hotel Manager";
+			console.log('gm');
+			break;
+		case 'kitchen':document.getElementById('notesGroup').innerHTML = "Kitchen";
+			console.log('kitchen');
+			break;
+		case 'meetings': document.getElementById('notesGroup').innerHTML = "Meetings";
+			console.log('meet');
+			break;
+		case 'reception':document.getElementById('notesGroup').innerHTML = "Reception";
+			console.log('recep');
+			break;
+		case 'restaurant':document.getElementById('notesGroup').innerHTML = "Restaurant";
+			console.log('restau');
+			break;
+
+
+	}
+	//let parent = document.getElementById('allnotes').parentElement.parentElement;
+	//document.getElementsByClassName('hm').innerHTML.value = "All notes";
+	//console.log(parent);
 }
